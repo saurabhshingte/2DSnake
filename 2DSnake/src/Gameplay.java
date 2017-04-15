0307a390e743922948784eb534ff72085b372531
Saurabh <Saurabh@SAURABH>
2017-04-15 22:40:32 +0530

    Game completed. Minor bugs remaining like freezing of screen.

--- 2DSnake/src/Gameplay.java
+++ 2DSnake/src/Gameplay.java
@@ -1,9 +1,11 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -20,6 +22,7 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
 	private boolean right = false;
 	private boolean up = false;
 	private boolean down = false;
+	//private boolean space = false;
 	
 	private int moves=0;
 	
@@ -34,6 +37,23 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
 	private int delay = 100;
 	
 	private ImageIcon snakeimage;
+
+	private int [] enemyxpos={25,50,75,100,125,150,175,200,225,250,275,300,325,
+			350,375,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,
+			800,850};
+	
+	private int [] enemyypos={75,100,125,150,175,200,225,250,275,300,325,
+			350,375,400,425,450,475,500,525,550,575,600,625};
+	
+	private ImageIcon enemyimage;
+	
+	private Random random = new Random(); 
+	
+	private int xpos = random.nextInt(32);	
+	private int ypos = random.nextInt(23);
+	
+	private int score = 0;
+	
 	
 	private ImageIcon titleImage;
 	
@@ -47,8 +67,7 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
 	}
 	
 	public void paint(Graphics g)
-	{
-		
+	{	
 		if(moves == 0)
 		{
 			snakexlength[2] = 50;
@@ -75,6 +94,16 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
 		//Draw background for the gameplay
 		g.setColor(Color.BLACK);
 		g.fillRect(25, 75, 850, 575);
+		
+		//Draw Scores
+		g.setColor(Color.white);
+		g.setFont(new Font("ariel", Font.PLAIN, 14));
+		g.drawString("Scores: "+ score, 780, 30);
+		
+		//Draw length
+		g.setColor(Color.white);
+		g.setFont(new Font("ariel", Font.PLAIN, 14));
+		g.drawString("Length: "+ lengthofsnake, 780, 50);
 		
 		//Draw Snake 
 		rightmouth = new ImageIcon("rightmouth.png");
@@ -114,18 +143,236 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
 			
 		}
 		
+		enemyimage = new ImageIcon("enemy.png");
+		
+		if((enemyxpos[xpos] == snakexlength[0] && enemyypos[ypos] == snakeylength[0]))
+		{
+			score++;
+			lengthofsnake++;
+			xpos = random.nextInt(34);
+			ypos = random.nextInt(23);
+		}
+		
+		enemyimage.paintIcon(this, g, enemyxpos[xpos], enemyypos[ypos]);
+		
+		for(int b = 1; b < lengthofsnake; b++)
+		{
+			if(snakexlength[b] == snakexlength[0] && snakeylength[b] == snakeylength[0])
+			{
+				right = false;
+				left = false;
+				up = false;
+				down = false;
+				
+				g.setColor(Color.WHITE);
+				g.setFont(new Font("arial", Font.BOLD,50));
+				g.drawString("Game Over", 300, 300);
+				
+				g.setFont(new Font("arial", Font.BOLD,20));
+				g.drawString("Space to Restart", 350, 340);
+			}
+		}
+		
+		g.dispose();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		timer.start();
+		
+		if(right)
+		{
+			for( int r = lengthofsnake-1; r>=0; r--)
+			{
+				snakeylength[r+1] = snakeylength[r];
+			}
+			
+			for(int r = lengthofsnake; r>=0; r--)
+			{
+				if(r==0)
+				{
+					snakexlength[r] = snakexlength[r] + 25;
+				}
+				else
+				{
+					snakexlength[r] = snakexlength[r-1];
+				}
+				if(snakexlength[r] > 850)
+				{
+					snakexlength[r] = 25;
+				}
+			}
+			repaint();
+		}
+		
+		if(left)
+		{
+			for( int r = lengthofsnake-1; r>=0; r--)
+			{
+				snakeylength[r+1] = snakeylength[r];
+			}
+			
+			for(int r = lengthofsnake; r>=0; r--)
+			{
+				if(r==0)
+				{
+					snakexlength[r] = snakexlength[r] - 25;
+				}
+				else
+				{
+					snakexlength[r] = snakexlength[r-1];
+				}
+				if(snakexlength[r] < 25)
+				{
+					snakexlength[r] = 850;
+				}
+			}
+			repaint();
+		}
+		
+		if(up)
+		{
+			for( int r = lengthofsnake-1; r>=0; r--)
+			{
+				snakexlength[r+1] = snakexlength[r];
+			}
+			
+			for(int r = lengthofsnake; r>=0; r--)
+			{
+				if(r==0)
+				{
+					snakeylength[r] = snakeylength[r] - 25;
+				}
+				else
+				{
+					snakeylength[r] = snakeylength[r-1];
+				}
+				if(snakeylength[r] < 75)
+				{
+					snakeylength[r] = 625;
+				}
+			}
+			repaint();
+		}
+		
+		if(down)
+		{
+			for( int r = lengthofsnake-1; r>=0; r--)
+			{
+				snakexlength[r+1] = snakexlength[r];
+			}
+			
+			for(int r = lengthofsnake; r>=0; r--)
+			{
+				if(r==0)
+				{
+					snakeylength[r] = snakeylength[r] + 25;
+				}
+				else
+				{
+					snakeylength[r] = snakeylength[r-1];
+				}
+				if(snakeylength[r] > 625)
+				{
+					snakeylength[r] = 75;
+				}
+			}
+			repaint();
+		}
 		
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void keyPressed(KeyEvent e)
+	{
+		if(e.getKeyCode() == KeyEvent.VK_SPACE)
+		{
+			moves=0;
+			score=0;
+			lengthofsnake=3;
+			repaint();
+		}
 		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+		{
+			moves++;
+			right = true;
+			
+			if(!left)
+			{
+				right = true;
+			}
+			
+			else
+			{
+				right = false;
+				left = true;
+			}
+			
+			up = false;
+			down = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_LEFT)
+		{
+			moves++;
+			left = true;
+			
+			if(!right)
+			{
+				left = true;
+			}
+			
+			else
+			{
+				left = false;
+				right = true;
+			}
+			
+			up = false;
+			down = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_UP)
+		{
+			moves++;
+			up = true;
+			
+			if(!down)
+			{
+				up = true;
+			}
+			
+			else
+			{
+				down = false;
+				up = true;
+			}
+			
+			left = false;
+			right = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_DOWN)
+		{
+			moves++;
+			down = true;
+			
+			if(!up)
+			{
+				down = true;
+			}
+			
+			else
+			{
+				up = false;
+				down = true;
+			}
+			
+			left = false;
+			right = false;
+		}
 		
 	}
 
